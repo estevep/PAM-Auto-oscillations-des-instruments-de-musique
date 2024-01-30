32c0d01f6b29ab5e9c6064428921e6701b7da268
Benjamin Quiédeville <84285984+BenjaminQuiedeville@users.noreply.github.com>
2024-01-30 15:48:51 +0100

    resolve error in VDP/main.py

--- Descripteurs/main.py
+++ Descripteurs/main.py
@@ -6,6 +6,7 @@ from matplotlib.widgets import Slider
 
 from scipy.io.wavfile import write, read
 from scipy.interpolate import CubicSpline
+from scipy.signal import welch
 
 import os 
 
@@ -22,11 +23,14 @@ def compute_offset_signal(signal: ArrayLike, deltaT: float, samplerate: float) -
     return interpolated_signal
 
 
-def import_signal(filepath: str, idx_begin=0, idx_end=None):
+def import_signal(filepath: str, time_begin=0, time_end=None):
     samplerate, signal = read(filepath)
 
     if signal.ndim > 1:
-        signal = signal[idx_begin:idx_end, 0]
+        signal = signal[:, 0]
+    
+    signal = signal[int(time_begin):int(time_end)]
+    signal = signal.astype("float") / np.max(signal)
 
     return samplerate, signal
 
@@ -35,26 +39,41 @@ def main() -> None:
 
     print(filenames)
 
-    samplerate, single_note_signal = import_signal(f"{samples_dir}/{filenames[1]}", 0, 1000)
-    _,          double_note_signal = import_signal(f"{samples_dir}/{filenames[2]}", 0, 1000)
+    samplerate, signal1 = import_signal(f"{samples_dir}/{filenames[1]}", 44100 * 3, 44100 * 3 + 1024)
+    _,          signal2 = import_signal(f"{samples_dir}/{filenames[2]}", 0, 1024)
     
 
     deltaT = 4e-4
-    single_note_interpolated_signal = compute_offset_signal(single_note_signal, deltaT, samplerate)
-    double_note_interpolated_signal = compute_offset_signal(double_note_signal, deltaT, samplerate)
+    signal1_interpolated = compute_offset_signal(signal1, deltaT, samplerate)
+    signal2_interpolated = compute_offset_signal(signal2, deltaT, samplerate)
+
+
+    fft_size = 1 << 15
+
+    signal1_dft = 20 * np.log10(np.abs(np.fft.rfft(signal1, fft_size)))
+    signal2_dft = 20 * np.log10(np.abs(np.fft.rfft(signal2, fft_size)))
+
+    freqs = np.fft.rfftfreq(fft_size, 1/samplerate)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    ax1.plot(freqs, signal1_dft)
+    ax1.set_xlim((0, 5000))
+    ax2.plot(freqs, signal2_dft)
+    ax2.set_xlim((0, 5000))
 
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
     
-    line1, = ax1.plot(single_note_signal, single_note_interpolated_signal)
+    line1, = ax1.plot(signal1, signal1_interpolated)
     ax1.spines[['left', 'bottom']].set_position('center')
     ax1.spines[['top', 'right']].set_visible(False)
-    ax1.set_title("une note")
+    # ax1.set_title("une note")
 
-    line2, = ax2.plot(double_note_signal, double_note_interpolated_signal)
+    line2, = ax2.plot(signal2, signal2_interpolated)
     ax2.spines[['left', 'bottom']].set_position('center')
     ax2.spines[['top', 'right']].set_visible(False)
-    ax2.set_title("deux notes")
+    # ax2.set_title("deux notes")
 
     axtau = fig.add_axes([0.25, 0.1, 0.65, 0.03])
     tau_slider = Slider(
@@ -66,8 +85,8 @@ def main() -> None:
     )
     
     def slider_update(val): 
-        line1.set_ydata(compute_offset_signal(single_note_signal, tau_slider.val, samplerate))
-        line2.set_ydata(compute_offset_signal(double_note_signal, tau_slider.val, samplerate))
+        line1.set_ydata(compute_offset_signal(signal1, tau_slider.val, samplerate))
+        line2.set_ydata(compute_offset_signal(signal2, tau_slider.val, samplerate))
 
     tau_slider.on_changed(slider_update)   
 
--- VDP/save.py
+++ VDP/save.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import os
+from os.path import isdir 
 from scipy.io.wavfile import write
+
 
 
 # Description : saves the pressure as a .wav file in the given directory
 def save_P(save_dir, Fs, P):
+    
+    if not isdir(save_dir): 
+        os.mkdir(save_dir)
+    
     files = [f for f in os.listdir(save_dir)]
     n = -1
     for f in files:
